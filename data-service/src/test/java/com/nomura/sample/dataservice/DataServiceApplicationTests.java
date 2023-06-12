d04cf2bbd0868de10b04457a212829a0dee436c1
Fujun Yang <fujun.yang@nomura.com>
2023-06-12 21:28:15 +0800

    Disable spring.cloud.kubernetes for DataServiceApplicationTests to fix test failure

--- data-service/src/test/java/com/nomura/sample/dataservice/DataServiceApplicationTests.java
+++ data-service/src/test/java/com/nomura/sample/dataservice/DataServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.nomura.sample.dataservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.cloud.kubernetes.enabled=false")
 class DataServiceApplicationTests {
 
 	@Test
